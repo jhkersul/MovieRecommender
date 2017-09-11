de5cbb8a272f8992e55194188eef6ecd8e2168ca
Kersul <joaohkfaria@gmail.com>
2017-09-11 19:24:26 -0300

    Removing favicon

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import path from 'path';
-import favicon from 'serve-favicon';
 import logger from 'morgan';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
@@ -14,8 +13,6 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
